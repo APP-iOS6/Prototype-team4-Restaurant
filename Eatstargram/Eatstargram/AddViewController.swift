9b3e8a80d394417a4d64e96c48dc26026e06b3bb
saul1113 <clclshtus@gmail.com>
2024-08-27 21:14:54 +0900

    AddView

--- Eatstargram/Eatstargram/AddViewController.swift
+++ Eatstargram/Eatstargram/AddViewController.swift
@@ -1,31 +1,363 @@
 //
-//  AddViewController.swift
-//  Eatstargram
+//  ViewController.swift
+//  AddView
 //
-//  Created by 박준영 on 8/27/24.
+//  Created by 강희창 on 8/27/24.
 //
 
 import UIKit
 
 class AddViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemBackground  
-        // Do any additional setup after loading the view.
+    
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        return scrollView
+    }()
+    // MARK: - title Stack
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "식당 등록"
+        label.font = UIFont.boldSystemFont(ofSize: 30)
+        return label
+    }()
+    
+    private var certifiButton: UIButton = {
+        let button = UIButton(type: .custom)
+        
+        var config = UIButton.Configuration.filled()
+        
+        config.title = "영수증 인증"
+        config.baseBackgroundColor = .systemBlue
+        config.baseForegroundColor = .white
+        config.cornerStyle = .medium // 버튼의 코너 스타일
+        config.imagePadding = 15 // 텍스트와 이미지 사이의 간격
+        config.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 16, bottom: 10, trailing: 16) // 패딩 설정
+        
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        
+        button.configuration = config
+        button.addAction(UIAction { [weak button] _ in
+            guard let button = button else { return }
+            
+            // 버튼의 상태 변경
+            let isChecked = button.tag == 0
+            button.tag = isChecked ? 1 : 0
+            
+            // 상태에 따른 UI 업데이트
+            if isChecked {
+                let checkmarkImage = UIImage(systemName: "checkmark")
+                button.configuration?.image = checkmarkImage
+                button.configuration?.baseForegroundColor = .white
+            } else {
+                button.configuration?.image = nil
+            }
+        }, for: .touchUpInside)
+        
+        
+        return button
+    }()
+    
+    private lazy var titleStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [titleLabel, certifiButton])
+        stackView.spacing = 10
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        
+        
+        return stackView
+    }()
+    // MARK: - TextFiedl Stack
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "이 름 :"
+        label.font = UIFont.boldSystemFont(ofSize: 24)
+        
+        return label
+    }()
+    
+    private lazy var nameTextField: UITextField = {
+        let textField = UITextField()
+        textField.backgroundColor = .lightGray
+        textField.textColor = .black
+        textField.clearsOnBeginEditing = false
+        
+        return textField
+    }()
+    
+    private lazy var nameStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [nameLabel, nameTextField])
+        stackView.spacing = 10
+        stackView.axis = .horizontal
+        stackView.distribution = .fillProportionally
+        stackView.alignment = .fill
+        return stackView
+    }()
+    
+    private let locationLabel: UILabel = {
+        let label = UILabel()
+        label.text = "장 소 :"
+        label.font = UIFont.boldSystemFont(ofSize: 24)
+        
+        return label
+    }()
+    
+    private lazy var locationTextField: UITextField = {
+        let textField = UITextField()
+        textField.backgroundColor = .lightGray
+        textField.textColor = .black
+        textField.clearsOnBeginEditing = false
+        
+        return textField
+    }()
+    
+    private lazy var locationStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [locationLabel, locationTextField])
+        stackView.spacing = 10
+        stackView.axis = .horizontal
+        stackView.distribution = .fillProportionally
+        return stackView
+    }()
+    
+    private lazy var topStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [nameStackView, locationStackView])
+        stackView.spacing = 18
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        
+        
+        
+        return stackView
+    }()
+    
+    private var locationButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 5
+        button.setTitle("현 위치", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        return button
+    }()
+    
+    private lazy var mapButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 5
+        button.setTitle("지도 검색", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        
+        let action = UIAction { _ in
+            let mapVC = MapModalViewController()
+            mapVC.modalPresentationStyle = .pageSheet
+            
+            
+            if let sheet = mapVC.sheetPresentationController {
+                sheet.detents = [
+                    .custom { _ in
+                        return UIScreen.main.bounds.height * 0.7
+                    }
+                ]
+            }
+            self.present(mapVC, animated: true, completion: nil)
+        }
+        
+        
+        button.addAction(action, for: .touchUpInside)
+        return button
+    }()
+    
+    
+    private lazy var buttonStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [locationButton, mapButton])
+        stackView.spacing = 10
+        stackView.axis = .horizontal
+        stackView.distribution = .fillProportionally
+        
+        
+        return stackView
+    }()
+    // MARK: - 이미지 스택
+    private let imageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "이미지"
+        label.font = UIFont.boldSystemFont(ofSize: 26)
+        return label
+    }()
+    
+    private lazy var addimageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .lightGray
+        return imageView
+    }()
+    
+    private var addImageButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.backgroundColor = .systemBlue
+        button.setTitle("이미지 추가", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        //        print("abc")
+        return button
+    }()
+    // MARK: - 갤러리 접근
+    private lazy var imagePickerController: UIImagePickerController = {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        return picker
+    }()
+    private lazy var addImageAction: UIAction = UIAction { [weak self] _ in
+        self?.present(self!.imagePickerController, animated: true)
+        //        print("aa")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private lazy var imageStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [imageLabel, addimageView, addImageButton])
+        stackView.spacing = 10
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.alignment = .fill
+        
+        return stackView
+    }()
+    
+    // MARK: - 소개글 Stack
+    private let infoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "소개글"
+        label.font = UIFont.boldSystemFont(ofSize: 26)
+        return label
+    }()
+    
+    private lazy var infoTextView: UITextView = {
+        let textView = UITextView()
+        textView.backgroundColor = .lightGray
+        return textView
+    }()
+    
+    private lazy var infoStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [infoLabel, infoTextView])
+        stackView.spacing = 10
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.alignment = .fill
+        
+        return stackView
+    }()
+    
+    private var registerButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.backgroundColor = .systemBlue
+        button.setTitle("등 록", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        
+        //등록 버튼을 누르면 dimiss
+        //        button.addAction(UIAction { [weak button] _ in
+        //             if let viewController = button?.superview?.next as? UIViewController {
+        //                 viewController.dismiss(animated: true, completion: nil)
+        //             }
+        //         }, for: .touchUpInside)
+        
+        return button
+    }()
+    // MARK: - ViewDidLoad
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+        setUpAutoLayout()
+        
     }
-    */
+    
+    
+    private func configure() {
+        view.backgroundColor = .systemBackground
+        [titleStackView, topStackView, buttonStackView, imageStackView, infoStackView, registerButton].forEach { scrollView.addSubview($0) }
+        view.addSubview(scrollView)
+        
+        
+        addImageButton.addAction(addImageAction, for: .touchUpInside)
+        
+    }
+    // MARK: - AutoLayOut
+    
+    private func setUpAutoLayout() {
+        nameTextField.translatesAutoresizingMaskIntoConstraints = false
+        locationTextField.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        titleStackView.translatesAutoresizingMaskIntoConstraints = false
+        topStackView.translatesAutoresizingMaskIntoConstraints = false
+        buttonStackView.translatesAutoresizingMaskIntoConstraints = false
+        addimageView.translatesAutoresizingMaskIntoConstraints = false
+        imageStackView.translatesAutoresizingMaskIntoConstraints = false
+        infoTextView.translatesAutoresizingMaskIntoConstraints = false
+        registerButton.translatesAutoresizingMaskIntoConstraints = false
+        infoStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            scrollView.widthAnchor.constraint(equalToConstant: view.frame.width),
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            
+        ])
+        
+        NSLayoutConstraint.activate([
+            titleStackView.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 20),
+            titleStackView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            titleStackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 20),
+            titleStackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -20),
+        ])
+        NSLayoutConstraint.activate([
+            nameTextField.leadingAnchor.constraint(equalTo: nameLabel.trailingAnchor, constant: -210),
+            locationTextField.leadingAnchor.constraint(equalTo: locationLabel.trailingAnchor, constant: -210),
+            topStackView.topAnchor.constraint(equalTo: titleStackView.bottomAnchor, constant: 30),
+            topStackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 20),
+            topStackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -20),
+        ])
+        
+        NSLayoutConstraint.activate([
+            buttonStackView.widthAnchor.constraint(equalToConstant: 200),
+            buttonStackView.topAnchor.constraint(equalTo: topStackView.bottomAnchor, constant: 20),
+            buttonStackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -20)
+        ])
+        
+        NSLayoutConstraint.activate([
+            addimageView.heightAnchor.constraint(equalToConstant: 150),
+            imageStackView.topAnchor.constraint(equalTo: topStackView.bottomAnchor, constant: 50),
+            imageStackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 20),
+            imageStackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -20)
+        ])
+        
+        NSLayoutConstraint.activate([
+            infoTextView.heightAnchor.constraint(equalToConstant: 150),
+            infoStackView.topAnchor.constraint(equalTo: imageStackView.bottomAnchor, constant: 20),
+            infoStackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 20),
+            infoStackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -20),
+            registerButton.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            registerButton.widthAnchor.constraint(equalToConstant: 80),
+            registerButton.topAnchor.constraint(equalTo: infoStackView.bottomAnchor, constant: 10)
+        ])
+        scrollView.contentSize = CGSize(
+            width: scrollView.frame.width,
+            height: scrollView.frame.height
+        )
+    }
+}
+// MARK: - Extension
 
+extension AddViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[.originalImage] as? UIImage {
+            addimageView.image = selectedImage
+        }
+        picker.dismiss(animated: true)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true)
+    }
 }
 
 #Preview {
